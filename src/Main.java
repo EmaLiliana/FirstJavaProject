085ec956c142ae1309e747fed69e91f407271a55
Liliana Dobre <lilianaemanueladobre@gmail.com>
2022-10-07 17:59:44 +0300

    Project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Liliana!");
+
+        byte myByteVariable = 2 + 5;
+       System.out.println(myByteVariable);
+
+       double myDoubleVariable = 5.0/ 2.0;
+        System.out.println(myDoubleVariable);
+        byte myNegativeNumber = -5 + 8 * 6;
+        System.out.println(myNegativeNumber);
+        byte myModuleOp = (55+9) % 9;
+        System.out.println(myModuleOp);
+        Byte myOpNumber = 20 + -3*5 / 8;
+        System.out.println(myOpNumber);
+        byte myLongOp = 5 + 15 / 3 * 2 -8 % 3;
+        System.out.println(myLongOp);
+
+
+
+
+
+
+
     }
 }
