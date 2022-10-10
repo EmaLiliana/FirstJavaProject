78e0240aada263e94cbec49dc009a5e6fa0dbc00
Liliana Dobre <lilianaemanueladobre@gmail.com>
2022-10-10 20:33:43 +0300

    Project

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,42 @@ public class Main {
         System.out.println("Hello Liliana!");
 
         byte myByteVariable = 2 + 5;
-       System.out.println(myByteVariable);
+        System.out.println(myByteVariable);
 
-       double myDoubleVariable = 5.0/ 2.0;
+        double myDoubleVariable = 5.0 / 2.0;
         System.out.println(myDoubleVariable);
         byte myNegativeNumber = -5 + 8 * 6;
         System.out.println(myNegativeNumber);
-        byte myModuleOp = (55+9) % 9;
+        byte myModuleOp = (55 + 9) % 9;
         System.out.println(myModuleOp);
-        byte myOpNumber = 20 + -3*5 / 8;
+        byte myOpNumber = 20 + -3 * 5 / 8;
         System.out.println(myOpNumber);
-        byte myLongOp = 5 + 15 / 3 * 2 -8 % 3;
+        byte myLongOp = 5 + 15 / 3 * 2 - 8 % 3;
         System.out.println(myLongOp);
 
+        int x = 2;
+        int y = 5;
+        int w = multiplication(x, y);
 
+        System.out.println(w);
 
+        float  a = substract (x,y);
+        System.out.println(a);
 
+    }
 
+    private static int multiplication(int first, int second) {
+        int result = first * second;
+        return result;
+    }
 
+    public static int substract(int first, int second) {
+        int result = first / second;
+        return result;
+    }
 
+    public static float substract(float first, float second) {
+        float result = first / second;
+        return result;
     }
 }
