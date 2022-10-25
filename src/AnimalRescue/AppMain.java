8709c26807fb5ea1389ec5f801bbc81eaa9aa0d0
Liliana Dobre <lilianaemanueladobre@gmail.com>
2022-10-25 04:17:25 +0300

    AnimalRescue

--- src/AnimalRescue/AppMain.java
+++ src/AnimalRescue/AppMain.java
@@ -3,33 +3,33 @@ package AnimalRescue;
 public class AppMain {
     public static void main(String[]arg) {
         Dog Charlie = new Dog();
-        System.out.println("The dog's name is: "+ Charlie.name);
+        System.out.println("The dog's name is: "+ Charlie.getName());
 
-        System.out.println("The dog's weight is: "+ Charlie.weight + "kg");
+        System.out.println("The dog's weight is: "+ Charlie.getWeight() + "kg");
 
-        System.out.println("The dog's color is: "+Charlie.color);
+        System.out.println("The dog's color is: "+Charlie.getColor());
 
-        System.out.println("The dog's age is: "+ Charlie.age);
+        System.out.println("The dog's age is: "+ Charlie.getAge());
 
-        System.out.println("The dog's breed is: "+ Charlie.breed);
+        System.out.println("The dog's breed is: "+ Charlie.getBreed());
 
-        System.out.println("The dog's happiness level is: "+ Charlie.happinessLevel);
+        System.out.println("The dog's happiness level is: "+ Charlie.getHappinessLevel());
 
-        System.out.println("The dog's gender is: "+ Charlie.gender);
+        System.out.println("The dog's gender is: "+ Charlie.getGender());
 
-        System.out.println("The dog's food preference is: "+ Charlie.foodPreference+ ","+Charlie.foodPrice + "ron," + Charlie.foodAvailability);
+        System.out.println("The dog's food preference is: "+ Charlie.getFoodPreference()+ ","+Charlie.getFoodPrice() + " ron," + Charlie.getFoodAvailability());
 
-        System.out.println("The dog's health level is: "+ Charlie.health);
+        System.out.println("The dog's health level is: "+ Charlie.getHealth());
 
-        System.out.println("The dog's appetite level is: "+ Charlie.appetite);
+        System.out.println("The dog's appetite level is: "+ Charlie.getAppetite());
 
-        System.out.println("The dog's favorite game is: "+ Charlie.favoriteGame);
+        System.out.println("The dog's favorite game is: "+ Charlie.getFavoriteGame());
 
-        System.out.println("The dog's activities are: "+Charlie.dogActivities);
-        System.out.println("Vet's name: " + Charlie.dogVet +", Specialization: " + Charlie.dogVetSpecialization);
+        System.out.println("The dog's activities are: "+Charlie.getDogActivities());
+        System.out.println("Vet's name: " + Charlie.getDogVet() +", Specialization: " + Charlie.getDogVetSpecialization());
 
 
-        Charlie.adopterNameMoney();
+
 
 
     }
--- src/AnimalRescue/Dog.java
+++ src/AnimalRescue/Dog.java
@@ -1,36 +1,149 @@
 package AnimalRescue;
 
 public class Dog {
-    String name = "Charlie";
-    String breed = "Golden Retriever";
-    int age = 10;
-    String color = "Golden";
-    double weight = 27.5;
-    String gender = "Male";
-    int appetite = 8;
-    int health = 10;
-    int happinessLevel = 10;
-    String foodPreference = "Taste of the Wild";
-    int foodPrice = 400;
-    int foodAvailability = 10;
-    String favoriteGame = "Ball game";
 
-    String dogActivities = "Walks, plays, eats, sleeps";
-
-    String dogVet ="Dr. Simona";
-
-    String dogVetSpecialization ="Gastroenterologist";
-
-    public void adopterNameMoney(){
-        System.out.println("Adopter name: Ema\nAvailable money for adoption: 1500lei ");
+    //name
+    private String name;
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
     }
 
+   //breed
+    private String breed;
+    public void setBreed(String breed){
+        this.breed = breed;
+    }
+    public String getBreed(){
+        return breed;
+    }
 
+    //age
+    private int age;
+    public void setAge(int age){
+        this.age= age;
+    }
+    public int getAge(){
+        return age;
+    }
 
+    //color
+    private String color;
+    public void setColor(String color){
+        this.color = color;
+    }
+    public String getColor(){
+        return color;
+    }
 
+    //weight
+    private double weight;
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+    public double getWeight(){
+        return weight;
+    }
 
+    //gender
+    private String gender;
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+    public String getGender(){
+        return gender;
+    }
 
+    //appetite
+    private int appetite;
+    public void setAppetite(int appetite){
+        this.appetite= appetite;
+    }
+    public int getAppetite(){
+        return appetite;
+    }
+    //Health
+    private int health;
+    public void setHealth(int health){
+        this.health= health;
+    }
+    public int getHealth(){
+        return health;
+    }
 
+    //Happiness Level
+    private int happinessLevel;
+    public void setHappinessLevel(int happinessLevel){
+        this.happinessLevel= happinessLevel;
+    }
+    public int getHappinessLevel(){
+        return happinessLevel;
+    }
+
+    //Food Preference
+    private String foodPreference;
+    public void setFoodPreference(String foodPreference){
+        this.foodPreference = foodPreference;
+    }
+    public String getFoodPreference(){
+        return foodPreference;
+    }
+
+    //Food Price
+    private float foodPrice;
+    public void setFoodPrice(float foodPrice){
+        this.foodPrice= foodPrice;
+    }
+    public float getFoodPrice(){
+        return foodPrice;
+    }
+
+    //Food Availability
+    private int foodAvailability;
+    public void setFoodAvailability(int foodAvailability){
+        this.foodAvailability = foodAvailability;
+    }
+    public int getFoodAvailability(){
+        return foodAvailability;
+    }
+
+    //Favorite Game
+    private String favoriteGame;
+    public void setFavoriteGame(String favoriteGame){
+        this.favoriteGame = favoriteGame;
+    }
+    public String getFavoriteGame(){
+        return favoriteGame;
+    }
+
+    //Dog Activities
+    private String dogActivities;
+    public void setDogActivities(String dogActivities){
+        this.dogActivities = dogActivities;
+    }
+    public String getDogActivities(){
+        return dogActivities;
+    }
+
+    //Dog'S VET
+    private String dogVet;
+    public void setDogVet(String dogVet){
+        this.dogVet = dogVet;
+    }
+    public String getDogVet(){
+        return dogVet;
+    }
+
+    //Vet's specialization
+    private String dogVetSpecialization;
+    public void setDogVetSpecialization(String dogVetSpecialization){
+        this.dogVetSpecialization = dogVetSpecialization;
+    }
+    public String getDogVetSpecialization(){
+        return dogVetSpecialization;
+    }
 
 
 
