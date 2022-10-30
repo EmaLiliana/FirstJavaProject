d852acc2268beb09cfec9adab20a4f1cd7135f76
Liliana Dobre <lilianaemanueladobre@gmail.com>
2022-10-30 13:40:38 +0200

    AnimalRescue

---
+++ src/Main4.java
@@ -0,0 +1,62 @@
+public class Main4 {
+
+
+    public static void main(String[] args) {
+        LogicalOp LogicalOp= new LogicalOp();
+
+        int z = 50;
+        LogicalOp.printFromNumberToNumber(z);
+
+
+
+
+        int j = (-50);
+        LogicalOp.printFromNumberToMinusNumber(j);
+
+
+        int x = 20;
+        int y = 50;
+        LogicalOp.counterFromTo(x,y);
+
+
+        int first= 20;
+        int last= 5;
+        LogicalOp.compareAndCount(first,last);
+
+
+
+
+        int n =1;
+        LogicalOp.evenNumber(n);
+
+
+
+
+        int a =1;
+        LogicalOp.oddNumber(a);
+
+
+
+
+
+        int sum = 0;
+        LogicalOp.sumOfNumberFromTo(sum);
+
+
+
+
+
+
+        LogicalOp.stars(n);
+
+
+
+
+
+
+
+
+
+    }
+    }
+
