91da2778448628ddffbea722a4b835fc7ce0037c
Liliana Dobre <lilianaemanueladobre@gmail.com>
2022-11-07 00:21:24 +0200

    Java Loops

--- src/LogicalOp.java
+++ src/LogicalOp.java
@@ -175,11 +175,11 @@ public class LogicalOp {
     // Metoda sa adune toate numerele pana la 100, pornind de la numarul primit ca si parametru,
     // iar la final sa returneze rezultatul.
     // Apelati metoda in clasa Main, metoda main() pentru a verifica daca functioneaza.
-    public int sumOfNumberFromTo(){
+    public int sumOfNumberFromTo() {
         int sum = 0;
 
-        for ( int i = 0; i<=100; i++ ){
-            sum = sum +i;
+        for (int i = 0; i <= 100; i++) {
+            sum = sum + i;
         }
         return sum;
 
@@ -202,7 +202,7 @@ public class LogicalOp {
     //*
     public void stars(int n) {
         n = 7;
-        for (int i = 1; i <=n ; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = i; j <= n; j++) {
                 System.out.print("*");
             }
@@ -213,7 +213,174 @@ public class LogicalOp {
 
 
     }
+
+    //Creati o metoda in clasa LogicalOp, care sa primeasca un numar ca si parametru. Metoda sa verifice numrul primit,
+    // si sa afiseze in consola o numaratoare pana la 100, pornind de la numarul primit ca si parametru.
+    public void numbFromTo(int numb) {
+        do {
+            System.out.println(numb);
+            numb++;
+        } while (numb <= 100);
+
+    }
+
+    // Creati o metoda in clasa LogicalOp, care sa primeste un numar ca si parametru. Metoda sa verifice numarul primit,
+    // si sa afiseze in consola o numaratoare pana la -100, pornind de la numarul primit ca si parametru.
+    public void minusNumber(int minus) {
+        do {
+            System.out.println(minus);
+            minus--;
+        } while (minus >= -100);
+    }
+
+    //Creati o metoda in clasa LogicalOp, care sa primeste doi parametrii de tip numar. Metoda sa afiseze o numaratoare intre
+    // cele doua numere primite, pornind de la primul parametru primit, pana la cel de-al doilea
+    // (ex: daca int x e primul parametru iar int y  este cel de-al doilea, numaratoarea sa se faca de la x, la y.
+    public void countfromToNum(int firstNum, int lastNum) {
+        do {
+            System.out.println(firstNum);
+            firstNum++;
+        } while (firstNum <= lastNum);
+    }
+
+    // Creati o metoda in clasa LogicalOp, care primeste doi parametrii de tip numar.
+    // Metoda trebuie sa verifice care dintre cele doua numere este mai mare, si sa execute o numaratoare
+    // crescatoare de la cel mai mic la cel mai mare.(ex: daca int x e primul parametru iar int y  este cel de-al doilea,
+    // daca x  este mai mare decat y,
+    // atunci numaratoarea sa fie de la y la x).
+    public void biggestNumbAndCount(int xx, int zz) {
+        if (zz > xx) {
+            do {
+                System.out.println(xx);
+                xx++;
+            } while (xx <= zz);
+
+
+        } else if (zz < xx) {
+            do {
+                System.out.println(zz);
+                zz++;
+            } while (zz <= xx);
+        }
+
+    }
+
+    //Creati o metoda in clasa LogicalOp care sa afiseze toate numerele pare de la 1 la 100
+    public void evenNumb(int i) {
+        for (int y = i; i <= 100; i++) {
+
+            do {
+                System.out.println(i);
+                i++;
+            }
+            while (i % 2 == 0);
+
+
+        }
+    }
+
+    // Creati o metoda in clasa LogicalOp care sa afiseze toate numerele impare de la 1 la 100}
+
+    public void oddNumb(int oddNumb) {
+        for (int i = oddNumb; oddNumb <= 100; oddNumb++) {
+            do {
+                System.out.println(oddNumb);
+                oddNumb++;
+            } while (oddNumb % 2 != 0);
+        }
+    }
+
+    //Creati o metoda care sa adune toate numerele din intervalul 111 si 8899. Sa se afiseze suma totala,
+    // iar dupa aceea sa se calculeze si afiseze media numerelor din acel interval.
+    // Introduceti o variabila int count in metoda, pentru a numara toate numerele din interval.
+    public float averageAndSum(int avgSum, int avgSumOf) {
+        float sum = 0;
+        int count = 0;
+        while (avgSum <= avgSumOf) {
+            sum = sum + avgSum;
+            count++;
+            avgSum++;
+        }
+        return sum / count;
+    }
+
+    //Creati o metoda care sa primeasca doi parametrii de tip numar,
+    // si sa returneze media numerelor divizibile cu 7, din acel interval.
+    public float divisibleNumbers(int c, int d) {
+        int sum = 0;
+        int count = 0;
+        for (int i = c; c <= d; c++) {
+            while (c % 7 == 0) {
+                sum = sum + c;
+                count++;
+                c++;
+            }
+
+        }
+        return sum / count;
+    }
+
+    //Creati o metoda care sa afiseze primele 20 de numere din sirul lui Fibonacci
+    public void fibonacci(int n1, int n2, int max) {
+
+        for (int i = 1; i <= max; i++) {
+            System.out.println(n1);
+            n1++;
+            int sum = n1 + n2;
+            n1 = n2;
+            n2 = sum;
+
+        }
+
+    }
+
+    // Creati o metoda numita CozaLozaWoza.
+//
+//Metoda va afisa:
+//
+//- numerele de la 1 la 110
+//
+//- 11 numere pe linie
+//
+//- se va afisa Coza pentru numerele care sunt multiplu de 3 (1 2 Coza 4)
+//
+//- se va afisa Loza pentru numerele care sunt multiplu de 5 (2 3 4 Loza 6)
+//
+//- se va afisa Woza pentru numerele care sunt multiplu de 7 (4 5 6 Woza 8)
+//
+//- se va afisa CozaLoza pentru numerele care sunt multiplu de 3 SI 5
+//
+//- se va afisa CozaWoza pentru multiplu de 3 SI 7
+//
+//- se va afisa WozaLoza pentru multiplu de 5 SI 7
+//
+//- se va afisa CozaLozaWoza pentru multiplu de 3 SI 5 SI 7
+//
+//Ar trebui sa arate similar:
+//
+//1 2 Coza 4 Loza Coza Woza 8 Coza Loza 11
+//
+//Coza 13 Woza CozaLoza 16 17 Coza 19 Loza CozaWoza 22
+//
+//23 Coza Loza 26 Coza Woza 29 CozaLoza 31 32 Coza
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/Main4.java
+++ src/Main4.java
@@ -40,14 +40,79 @@ public class Main4 {
 
 
 
-        System.out.println(LogicalOp.sumOfNumberFromTo());;
-
+        System.out.println(LogicalOp.sumOfNumberFromTo());
 
 
 
 
 
         LogicalOp.stars(n);
+
+
+
+        //while
+        int numb=0;
+        LogicalOp.numbFromTo(numb);
+
+
+        int minus=0;
+        LogicalOp.minusNumber(minus);
+
+
+        int firstNum=1;
+        int lastNum = 20;
+        LogicalOp.countfromToNum(firstNum,lastNum);
+
+
+        int xx= 5;
+        int zz= 10;
+        LogicalOp.biggestNumbAndCount(xx,zz);
+
+
+        int i =0;
+        LogicalOp.evenNumb(i);
+
+        int oddNumb =0;
+        LogicalOp.oddNumb(oddNumb);
+
+
+        int avgSum=111;
+        int  avgSumOf= 8899;
+        System.out.println(LogicalOp.averageAndSum(avgSum,avgSumOf));
+
+
+
+
+        int c= 7;
+        int d= 35;
+        System.out.println(LogicalOp.divisibleNumbers(c,d));
+
+
+
+
+        int max = 20;
+        int n1 = 0;
+        int n2 = 1;
+
+        LogicalOp.fibonacci(n1,n2,max);
+
+
+
+        int itemsCount= 0;
+        int itemsPerLine=0;
+
+
+
+
+        //LogicalOp.cozaLozaWoza( itemsCount, itemsPerLine);
+
+
+
+
+
+
+
+
 
 
 
